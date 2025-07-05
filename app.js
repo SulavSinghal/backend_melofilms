02b5a5a6de12fdf60195caab2361711e35dd2a5b
Sulav Singhal <sulavsinghal01@gmail.com>
2025-07-05 16:52:11 +0530

    Update app.js
    added limit

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ connectDB();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Serve static files
 
 // Routes
